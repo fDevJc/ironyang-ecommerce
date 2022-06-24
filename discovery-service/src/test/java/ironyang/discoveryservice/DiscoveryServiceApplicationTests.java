5ffdb4c993c9f53bb3a6d9d5f08472e2971784f7
fDevJc <fdevjc@gmail.com>
2022-06-24 11:44:02 +0900

    init discovery-service

---
+++ discovery-service/src/test/java/ironyang/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ironyang.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

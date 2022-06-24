78fc952d6e195f6a28ed528cd4f20991b2fd718f
fDevJc <fdevjc@gmail.com>
2022-06-24 13:01:10 +0900

    delete ecommerce package

--- ecommerce/src/main/java/ironyang/ecommerce/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package ironyang.ecommerce.repository;
-
-import ironyang.ecommerce.domain.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
